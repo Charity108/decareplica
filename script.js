9254ea90468f1c5852c37ebe1879ce55bdd7200a
Charity <ucharity91@gmail.com>
2022-01-01 22:38:53 -0800

    added a hamburger menu

--- script.js
+++ script.js
@@ -16,3 +16,26 @@ const observer = new IntersectionObserver((entries) => {
 }, options);
 
 observer.observe(header);
+
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const navu = document.querySelector(".ull");
+  const navLinks = document.querySelectorAll(".ull li");
+  console.log(navLinks);
+  // toggle nav
+  burger.addEventListener("click", () => {
+    navu.classList.toggle("nav-active");
+
+    // animate nav
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.3
+        }s`;
+      }
+    });
+  });
+};
+navSlide();
